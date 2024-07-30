122c495fe11c4850a20ec6d25dbdbc4d3c1faf61
nil3sh99 <agnilesh8199@gmail.com>
2024-07-30 19:12:14 +0000

    pushing updated gen-ai application code

--- gen-ai/app1/index.js
+++ gen-ai/app1/index.js
@@ -1,18 +1,47 @@
-import express from 'express';
+// import express from 'express';
 
-// create a new application instance
+// // create a new application instance
+// const app = express();
+
+
+// app.get('/', async (req, res) => {
+//     res.send('Hello world');
+// })
+
+// // env variable - choose between cloud run env or local host
+// const port = parseInt(process.env.PORT) || 8080;
+
+// // run the application
+// app.listen(port, () => {
+//     // ` ` backticks allows us to use env variable
+//     console.log(`codelab-genai: listening on port ${port}`);
+// })
+
+// modified code for gen-ai application
+import express from 'express';
 const app = express();
 
+import { VertexAI } from '@google-cloud/vertexai';
+
+import { GoogleAuth } from 'google-auth-library';
+const auth = new GoogleAuth();
 
 app.get('/', async (req, res) => {
-    res.send('Hello world');
-})
+    const project = await auth.getProjectId();
 
-// env variable - choose between cloud run env or local host
+    const vertex = new VertexAI({ project: project });
+    const generativeModel = vertex.getGenerativeModel({
+        model: 'gemini-1.5-flash'
+    });
+
+    const animal = req.query.animal || 'dog';
+    const prompt = `Give me 10 fun facts about ${animal}. Return this as html without backticks.`
+    const resp = await generativeModel.generateContent(prompt);
+    const html = resp.response.candidates[0].content.parts[0].text;
+    res.send(html);
+});
+
 const port = parseInt(process.env.PORT) || 8080;
-
-// run the application
 app.listen(port, () => {
-    // ` ` backticks allows us to use env variable
     console.log(`codelab-genai: listening on port ${port}`);
-})
+});
